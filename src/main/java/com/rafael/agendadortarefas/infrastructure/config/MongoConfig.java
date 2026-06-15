1b249164938539f4b688de39f099e03882846166
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-06-15 17:43:37 -0300

    refactor: configuração explícita do MongoDB + ajustes de segurança e application.yaml

--- src/main/java/com/rafael/agendadortarefas/infrastructure/config/MongoConfig.java
+++ src/main/java/com/rafael/agendadortarefas/infrastructure/config/MongoConfig.java
@@ -1,4 +1,29 @@
 package com.rafael.agendadortarefas.infrastructure.config;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
 public class MongoConfig {
+
+    @Bean
+    @Primary
+    public MongoDatabaseFactory mongoDatabaseFactory() {
+        // Força o uso do database db_agendador
+        return new SimpleMongoClientDatabaseFactory(
+                MongoClients.create("mongodb://localhost:27017/db_agendador"),
+                "db_agendador"
+        );
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDatabaseFactory());
+    }
 }
