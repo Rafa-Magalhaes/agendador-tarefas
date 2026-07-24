aacc736843ebdd023febb35d73328ee2ae926dc9
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-07-24 01:49:26 -0300

    Feature/ajuste seguranca swagger agendador (#14)
    * chore(git): adiciona application-dev.yml ao .gitignore
    
    * feat(agendador): ajusta validacao do service token (tokentype), implementa Swagger e atualiza config JWT

--- src/test/java/com/rafael/agendadortarefas/AgendadorTarefasApplicationTests.java
+++ src/test/java/com/rafael/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -3,7 +3,10 @@ package com.rafael.agendadortarefas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"jwt.secret=chave-falsa-para-testes-no-ci-nao-usar-em-producao",
+		"spring.data.mongodb.uri=mongodb://localhost:27017/db_agendador"
+})
 class AgendadorTarefasApplicationTests {
 
 	@Test
